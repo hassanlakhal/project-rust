1ad60c02b4576cd24bb364dea21e4570e92a8ca5
lakhalhassan <lakehalhassan2120@gmail.com>
2024-10-30 15:07:23 +0100

    tt

--- hello-word/hello.rs
+++ hello-word/hello.rs
@@ -1,3 +1,18 @@
+
+
 fn main(){
-    println!("hello word");
+
+
+
+    let company_string = "TutorialsPoint";  // string type
+    let rating_float = 4.5;                 // float type
+    let is_growing_boolean = true;          // boolean type
+    let icon_char = '♥';                    //unicode character type
+    let mut test = 1337_0; 
+    println!("hello word test {}",test);
+    test = 0;
+    println!("hello word test {}",test);
+    println!("-----------------------------");
+    println!("{} | {} | {} |{} | ",company_string,rating_float,is_growing_boolean,icon_char);
+
 }
