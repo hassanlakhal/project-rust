af412662816d6be77caa941ad2830b16d18c54bf
Hassan Lakhal <hlakhal-@e1r3p11.1337.ma>
2024-12-31 16:24:47 +0100

    add cargo in this project

---
+++ hello-word/first_project/target/debug/deps/first_project-7b1b9e43eff05dd7.d
@@ -0,0 +1,5 @@
+/home/hlakhal-/Desktop/project-rust/hello-word/first_project/target/debug/deps/first_project-7b1b9e43eff05dd7: src/main.rs
+
+/home/hlakhal-/Desktop/project-rust/hello-word/first_project/target/debug/deps/first_project-7b1b9e43eff05dd7.d: src/main.rs
+
+src/main.rs:
---
+++ hello-word/first_project/target/debug/first_project.d
@@ -0,0 +1,1 @@
+/home/hlakhal-/Desktop/project-rust/hello-word/first_project/target/debug/first_project: /home/hlakhal-/Desktop/project-rust/hello-word/first_project/src/main.rs
